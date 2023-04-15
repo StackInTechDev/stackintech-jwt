2f6cbd28f23dcc107cb60c69e9d4c812538d5c66
Dahmane13 <dahmane.abdelmalek13@gmail.com>
2023-04-15 19:16:08 +0100

    Add common module for validation, error handling

---
+++ src/common/common.service.ts
@@ -0,0 +1,103 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  LoggerService,
+  NotFoundException,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { ObjectLiteral, EntityManager, QueryRunner } from 'typeorm';
+import { isNull, isUndefined } from './utils/validation.util';
+import slugify from 'slugify';
+import { v4 } from 'uuid';
+import { IMessage } from './interfaces/message.interface';
+
+@Injectable()
+export class CommonService {
+  private readonly loggerService: LoggerService;
+  constructor() {
+    this.loggerService = new Logger(CommonService.name);
+  }
+
+  public async validateEntity(entity: ObjectLiteral): Promise<void> {
+    const errors = await validate(entity);
+    const messages: string[] = [];
+
+    for (const error of errors) {
+      messages.push(...Object.values(error.constraints));
+    }
+
+    if (errors.length > 0) {
+      throw new BadRequestException(messages.join(',\n'));
+    }
+  }
+
+  public async throwDuplicateError<T>(promise: Promise<T>, message?: string) {
+    try {
+      return await promise;
+    } catch (error) {
+      this.loggerService.error(error);
+
+      if (error.code === '23505') {
+        throw new ConflictException(message ?? 'Duplicated value in database');
+      }
+
+      throw new BadRequestException(error.message);
+    }
+  }
+  public async throwInternalError<T>(promise: Promise<T>): Promise<T> {
+    try {
+      return await promise;
+    } catch (error) {
+      this.loggerService.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  public checkEntityExistence<T extends ObjectLiteral>(
+    entity: T | null | undefined,
+    name: string,
+  ): void {
+    if (isNull(entity) || isUndefined(entity)) {
+      throw new NotFoundException(`${name} not found`);
+    }
+  }
+
+  public async saveEntity<T extends ObjectLiteral>(
+    manager: EntityManager,
+    entity: T,
+    isNew = false,
+  ): Promise<void> {
+    await this.validateEntity(entity);
+    const queryRunner: QueryRunner = manager.queryRunner;
+    await queryRunner.startTransaction();
+    if (isNew) {
+      await queryRunner.manager.save(entity);
+    }
+    await this.throwDuplicateError(queryRunner.commitTransaction());
+  }
+
+  public async removeEntity<T extends ObjectLiteral>(
+    manager: EntityManager,
+    entity: T,
+  ): Promise<void> {
+    await this.throwInternalError(manager.remove(entity));
+  }
+
+  public formatName(title: string): string {
+    return title
+      .trim()
+      .replace(/\n/g, ' ')
+      .replace(/\s\s+/g, ' ')
+      .replace(/\w\S*/g, (w) => w.replace(/^\w/, (l) => l.toUpperCase()));
+  }
+  public generatePointSlug(str: string): string {
+    return slugify(str, { lower: true, replacement: '.', remove: /['_\.\-]/g });
+  }
+
+  public generateMessage(message: string): IMessage {
+    return { id: v4(), message };
+  }
+}
