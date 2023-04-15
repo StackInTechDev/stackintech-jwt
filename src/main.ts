1fd135b5388dad0e5d40d526264b6c82a995f75f
Dahmane13 <dahmane.abdelmalek13@gmail.com>
2023-04-15 19:13:33 +0100

    fix merge conflict

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-
-
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('StackIntech JWT')
-    .setDescription('"stackintech-jwt" is a NestJS open-source project available on GitHub that provides a simple and secure way to implement JSON Web Tokens (JWT) for authentication and authorization in your NestJS applications.')
+    .setDescription(
+      '"stackintech-jwt" is a NestJS open-source project available on GitHub that provides a simple and secure way to implement JSON Web Tokens (JWT) for authentication and authorization in your NestJS applications.',
+    )
     .setVersion('1.0')
     .addTag('JWT')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
